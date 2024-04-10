ca5d3f2a37d0e738469ff59dd14f22cb9bab6e19
Alex Marin Mendez <alexmarinmendez@gmail.com>
2024-04-10 04:39:58 -0500

    Agregar archivos de proyecto.

---
+++ CursoEF/Data/ApplicationDBContext.cs
@@ -0,0 +1,17 @@
+﻿using CursoEF.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CursoEF.Data
+{
+    public class ApplicationDBContext: DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options): base(options)
+        {
+            
+        }
+
+        // Aquí van los modelos
+        public DbSet<Categoria> Categoria { get; set; }
+        public DbSet<Usuario> Usuario { get; set; }
+    }
+}
---
+++ CursoEF/Program.cs
@@ -0,0 +1,29 @@
+using CursoEF.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Configurar la conexión a SQLServer
+builder.Services.AddDbContext<ApplicationDBContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ConexionSql")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
